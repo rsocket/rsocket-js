90b9b4a1f9753f064fa1b2c731ff5050fc6287a7
Kevin Viglucci <kviglucci@gmail.com>
2021-07-08 11:24:15 -0500

    chore: cleanup template files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,2 +1,11 @@
 module.exports = {
+  extends: "@nighttrax/eslint-config-tsx",
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
 };
