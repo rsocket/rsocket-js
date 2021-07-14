6ee2e13529082eec70609134f3755e22b1591caa
Kevin Viglucci <kviglucci@gmail.com>
2021-07-14 09:55:48 -0500

    refactor: move sendOne to client proto to avoid extending TcpConnection

--- packages/rsocket-tcp-client/example/index.ts
+++ packages/rsocket-tcp-client/example/index.ts
@@ -4,23 +4,22 @@ import { RSocketFrameFactories } from "@rsocket/rsocket-core";
 
 // eslint-disable-next-line consistent-return
 const run = async () => {
-
   const connectionOptions = {
     host: "localhost",
     port: 9090,
   };
-  let transport = null;
+
+  const transport = new RSocketTcpClient(connectionOptions);
 
   try {
-    transport = await new RSocketTcpClient(connectionOptions).connect();
+    await transport.connect();
     console.log("TCP connection established...");
+    const setupFrame = RSocketFrameFactories.CreateSetupFrame();
+    transport.sendOne(setupFrame);
   } catch (err) {
     console.error(err);
     return process.exit(1);
   }
-
-  const setupFrame = RSocketFrameFactories.CreateSetupFrame();
-  transport.sendOne(setupFrame);
 };
 
 run();
