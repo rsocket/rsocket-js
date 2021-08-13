179ccc6c572c3471e0dda6e13b4c90dac6dda503
Kevin Viglucci <kviglucci@gmail.com>
2021-08-12 23:32:02 -0500

    fix: compilation errors in tcp server tests
    Signed-off-by: Kevin Viglucci <kviglucci@gmail.com>

---
+++ packages/rsocket-tcp-server/src/__tests__/TcpClientTransport.spec.ts
@@ -0,0 +1,34 @@
+import { TcpServerTransport } from "../TcpServerTransport";
+// import { TcpDuplexConnection } from "../TcpDuplexConnection";
+import * as net from "net";
+// import sinon from "sinon";
+// import EventEmitter from "events";
+// import { DuplexConnection } from "@rsocket/rsocket-core/src";
+
+describe("TcpClientTransport", function () {
+  describe("connect", () => {
+    it.skip("resolves to an instance of DuplexConnection on successful connection", async () => {
+      // arrange
+      // const netStub = new EventEmitter();
+      // const socketStub = sinon.createStubInstance(net.Socket);
+
+      const transport = new TcpServerTransport({
+        listenOptions: undefined,
+        serverOptions: undefined,
+        socketCreator(options: net.ServerOpts): net.Server {
+          return undefined;
+        },
+      });
+
+      // act
+      const connectionPromise = await transport.bind((connection) => {});
+
+      // netStub.emit("connect", socketStub);
+      //
+      // // assert
+      // await expect(connectionPromise).resolves.toBeInstanceOf(
+      //   TcpDuplexConnection
+      // );
+    });
+  });
+});
