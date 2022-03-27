10fad7d9d0101d34e4632d7abe64a1bed28a40ae
Kevin Viglucci <kviglucci@gmail.com>
2022-03-27 00:50:06 -0500

    feat: setup resolver for chat like subscriptions
    Signed-off-by: Kevin Viglucci <kviglucci@gmail.com>

--- packages/rsocket-examples/src/graphql/apollo/resolvers.ts
+++ packages/rsocket-examples/src/graphql/apollo/resolvers.ts
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { PubSub } from "graphql-subscriptions";
+
+const pubsub = new PubSub();
 
 export const resolvers = {
   Query: {
@@ -23,20 +26,20 @@ export const resolvers = {
       };
     },
   },
+  Mutation: {
+    createMessage: async (_, { message }, context, info) => {
+      await pubsub.publish("POST_CREATED", {
+        messageCreated: {
+          message,
+        },
+      });
+    },
+  },
   Subscription: {
-    echo: {
+    messageCreated: {
       // subscribe must return an AsyncIterator
       // https://www.apollographql.com/docs/apollo-server/data/subscriptions/#resolving-a-subscription
-      subscribe: async function* (parent, args, context, info) {
-        const { message } = args;
-        for await (const num of [1, 2, 3]) {
-          yield {
-            echo: {
-              message: `${message} ${num}`,
-            },
-          };
-        }
-      },
+      subscribe: () => pubsub.asyncIterator(["POST_CREATED"]),
     },
   },
 };
