7a5c6ff26216bc7fa8e4dd3632aa62d1a2c99c54
Kevin Viglucci <kviglucci@gmail.com>
2022-05-01 21:23:59 -0500

    chore: add testing example for calling a spring boot graphql server
    Signed-off-by: Kevin Viglucci <kviglucci@gmail.com>

---
+++ packages/rsocket-examples/src/graphql/apollo/spring-example.ts
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2021-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { RSocket, RSocketConnector } from "rsocket-core";
+import { makeRSocketLink } from "rsocket-graphql-apollo-link";
+import { WebsocketClientTransport } from "rsocket-websocket-client";
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+import gql from "graphql-tag";
+import WebSocket from "ws";
+import { exit } from "process";
+import { WellKnownMimeType } from "rsocket-composite-metadata";
+
+let rsocketClient: RSocket;
+
+function makeRSocketConnector() {
+  return new RSocketConnector({
+    setup: {
+      // TODO: dataMimeType value should come from WellKnownMimeType
+      dataMimeType: "application/graphql+json",
+      metadataMimeType:
+        WellKnownMimeType.MESSAGE_RSOCKET_COMPOSITE_METADATA.toString(),
+    },
+    transport: new WebsocketClientTransport({
+      url: "ws://localhost:7000/rsocket",
+      wsCreator: (url) => new WebSocket(url) as any,
+    }),
+  });
+}
+
+function makeApolloClient({ rsocketClient }) {
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: makeRSocketLink({
+      rsocket: rsocketClient,
+      endpoint: "graphql",
+    }),
+  });
+}
+
+async function main() {
+  // client setup
+  const connector = makeRSocketConnector();
+  rsocketClient = await connector.connect();
+
+  const apolloClient = makeApolloClient({ rsocketClient });
+
+  const greeting = await apolloClient.query({
+    variables: {},
+    query: gql`
+      query greeting {
+        greeting
+      }
+    `,
+  });
+
+  console.log(greeting);
+
+  const echo = await apolloClient.query({
+    variables: {
+      input: "Hello World",
+    },
+    query: gql`
+      query echo($input: String) {
+        echo(input: $input) {
+          message
+        }
+      }
+    `,
+  });
+
+  console.log(echo);
+}
+
+main()
+  .catch((error: Error) => {
+    console.error(error);
+    exit(1);
+  })
+  .finally(async () => {
+    rsocketClient.close();
+  });
