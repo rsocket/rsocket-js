01e3e2cb4b95d3725eecce16683b66226e4b68b7
Kevin Viglucci <kviglucci@gmail.com>
2022-06-12 09:43:52 -0500

    refactor: remove unusd variables from test-utils
    Signed-off-by: Kevin Viglucci <kviglucci@gmail.com>

--- packages/rsocket-composite-metadata/__tests__/test-utils/hex.ts
+++ packages/rsocket-composite-metadata/__tests__/test-utils/hex.ts
@@ -15,8 +15,6 @@ function strHex(s) {
   return a;
 }
 
-const numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-// const numericChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const alphabetNumeric = "abcdefghijklmnopqrstuvqxyz0123456789";
 
 export const hex: any = {};
